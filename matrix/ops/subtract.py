3abf27178b45851d60f063b8d0494a304e4e2560
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-11-09 17:26:21 +0000

    Add and test subtract

---
+++ matrix/ops/subtract.py
@@ -0,0 +1,22 @@
+import lab as B
+from plum import promote
+
+from ..matrix import AbstractMatrix, Dense
+from ..diagonal import Diagonal
+
+__all__ = []
+
+
+@B.dispatch({B.Numeric, AbstractMatrix}, {B.Numeric, AbstractMatrix})
+def subtract(a, b):
+    return B.subtract(*promote(a, b))
+
+
+@B.dispatch(Dense, Dense)
+def subtract(a, b):
+    return Dense(B.subtract(a.mat, b.mat))
+
+
+@B.dispatch(Diagonal, Diagonal)
+def subtract(a, b):
+    return Diagonal(B.subtract(a.diag, b.diag))
