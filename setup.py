a74d106c1760df93ac44ad278d215f9cbdbee536
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-11-09 15:12:15 +0000

    Update requirements and .travis.yml

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import find_packages, setup
 
-requirements = ['numpy>=1.16']
+requirements = ['numpy>=1.16',
+
+                'backends',
+                'plum-dispatch']
 
 setup(packages=find_packages(exclude=['docs']),
       python_requires='>=3.6',
