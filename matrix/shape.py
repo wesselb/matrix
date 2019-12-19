537ca910780ef7c70e3dc1db97941231be2ba394
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-19 17:29:23 +0100

    Add and test shape utils

---
+++ matrix/shape.py
@@ -0,0 +1,183 @@
+import lab as B
+from plum import Dispatcher
+
+__all__ = ['assert_scalar',
+           'assert_vector',
+           'assert_matrix',
+           'get_shape',
+           'compatible',
+           'assert_compatible',
+           'broadcast']
+
+dispatch = Dispatcher()
+
+
+def assert_scalar(x, message):
+    """Assert that a tensor is a scalar.
+
+    Args
+        x (tensor): Tensor that must be a scalar.
+        message (str): Error message to raise in the case that `x` is not a
+            scalar.
+    """
+    if B.rank(x) != 0:
+        raise AssertionError(message)
+
+
+def assert_vector(x, message):
+    """Assert that a tensor is a vector.
+
+    Args
+        x (tensor): Tensor that must be a vector.
+        message (str): Error message to raise in the case that `x` is not a
+            vector.
+    """
+    if B.rank(x) != 1:
+        raise AssertionError(message)
+
+
+def assert_matrix(x, message):
+    """Assert that a tensor is a matrix.
+
+    Args
+        x (tensor): Tensor that must be a matrix.
+        message (str): Error message to raise in the case that `x` is not a
+            matrix.
+    """
+    if B.rank(x) != 2:
+        raise AssertionError(message)
+
+
+class Dimension:
+    """Dimension of a shape.
+
+    Args:
+        size (int): Size of the dimension.
+    """
+
+    def __init__(self, size):
+        self.size = size
+
+    def __eq__(self, other):
+        return self.size == other.size
+
+    def __str__(self):
+        return str(self.size)
+
+    def __repr__(self):
+        return f'Dimension({self.size})'
+
+    def __int__(self):
+        return self.size
+
+
+class Shape:
+    """A shape, which comprises dimensions.
+
+    Args:
+        *dims (:class:`.shape.Dimension`): Dimensions of the shape.
+    """
+
+    def __init__(self, *dims):
+        self.dims = dims
+        self.rank = len(dims)
+
+    def __eq__(self, other):
+        return self.dims == other.dims
+
+    def __str__(self):
+        return '({})'.format(', '.join(str(d) for d in self.dims))
+
+    def __repr__(self):
+        return 'Shape({})'.format(', '.join(repr(d) for d in self.dims))
+
+    def __getitem__(self, item):
+        return int(self.dims[item])
+
+    def as_tuple(self):
+        return tuple(d.size for d in self.dims)
+
+    def __iter__(self):
+        return iter(self.as_tuple())
+
+
+@dispatch([object])
+def get_shape(*xs):
+    """Get the shapes of objects as :class:`.shape.Shape` objects.
+
+    Args:
+        *xs (object): Objects to get the shape of.
+
+    Returns:
+        tuple: Tuple containing the shape objects.
+    """
+    return tuple(get_shape(x) for x in xs)
+
+
+@dispatch(object)
+def get_shape(x):
+    return Shape(*[Dimension(d) for d in B.shape(x)])
+
+
+@dispatch(object, object)
+def compatible(x1, x2):
+    """Assert that two objects have compatible shapes.
+
+    Args:
+        x1 (object): First object.
+        x2 (object): Second object.
+
+    Returns:
+        bool: Boolean indicating whether the two objects have compatible shapes.
+    """
+    return compatible(*get_shape(x1, x2))
+
+
+@dispatch(Shape, Shape)
+def compatible(s1, s2):
+    return s1.rank == s2.rank and \
+           all(compatible(d1, d2) for d1, d2 in zip(s1.dims, s2.dims))
+
+
+@dispatch(Dimension, Dimension)
+def compatible(d1, d2):
+    return d1.size == 1 or d2.size == 1 or d1.size == d2.size
+
+
+def assert_compatible(x1, x2):
+    """Assert that two objects have compatible shapes.
+
+    Args:
+        x1 (object): First object.
+        x2 (object): Second object.
+    """
+    if not compatible(x1, x2):
+        raise AssertionError(f'Shapes {x1} and {x2} are asserted to be '
+                             f'compatible, but they are not.')
+
+
+@dispatch(object, object)
+def broadcast(x1, x2):
+    """Broadcast the shapes of two objects.
+
+    Args:
+        x1 (object): First object.
+        x2 (object): Second object.
+
+    Returns:
+        :class:`.shape.Shape`: Broadcasted shape.
+    """
+    return broadcast(*get_shape(x1, x2))
+
+
+@dispatch(Shape, Shape)
+def broadcast(s1, s2):
+    assert_compatible(s1, s2)
+    return Shape(*(broadcast(d1, d2) for d1, d2 in zip(s1.dims, s2.dims)))
+
+
+@dispatch(Dimension, Dimension)
+def broadcast(d1, d2):
+    if not compatible(d1, d2):
+        raise RuntimeError(f'Cannot broadcast dimensions {d1} and {d2}.')
+    return d2 if int(d1) == 1 else d1
---
+++ tests/test_shape.py
@@ -0,0 +1,84 @@
+import lab as B
+import pytest
+
+from matrix.shape import (
+    assert_scalar,
+    assert_vector,
+    assert_matrix,
+    Dimension,
+    Shape,
+    compatible,
+    assert_compatible,
+    broadcast
+)
+# noinspection PyUnresolvedReferences
+from .util import dense1
+
+
+def test_assert_scalar():
+    assert_scalar(1, 'test')
+
+    with pytest.raises(AssertionError):
+        assert_scalar(B.ones(3), 'test')
+
+
+def test_assert_vector():
+    assert_vector(B.ones(3), 'test')
+
+    with pytest.raises(AssertionError):
+        assert_vector(1, 'test')
+
+
+def test_assert_matrix():
+    assert_matrix(B.ones(3, 3), 'test')
+
+    with pytest.raises(AssertionError):
+        assert_matrix(B.ones(3), 'test')
+
+
+def test_dimension():
+    d = Dimension(5)
+
+    assert d.size == 5
+    assert d == d
+    assert d != Dimension(1)
+    assert d != Dimension(3)
+    assert str(d) == '5'
+    assert repr(d) == 'Dimension(5)'
+    assert int(d) is 5
+
+
+def test_shape():
+    s = Shape(Dimension(5), Dimension(4))
+
+    assert s.rank == 2
+    assert s == s
+    assert s != Shape(Dimension(5), Dimension(1))
+    assert s != Shape(Dimension(5), Dimension(3))
+    assert str(s) == '(5, 4)'
+    assert repr(s) == 'Shape(Dimension(5), Dimension(4))'
+    assert s[0] == 5
+    assert s[1] == 4
+    assert s.as_tuple() == (5, 4)
+    assert tuple(iter(s)) == (5, 4)
+
+
+def test_compatible():
+    assert compatible(B.ones(5, 5), B.ones(5, 5))
+    assert compatible(B.ones(5, 5), B.ones(5, 1))
+    assert not compatible(B.ones(5, 5), B.ones(5))
+    assert not compatible(B.ones(5, 5), B.ones(5, 4))
+
+    with pytest.raises(AssertionError):
+        assert_compatible(B.ones(5), B.ones(4))
+
+
+def test_broadcast():
+    s = Shape(Dimension(5), Dimension(4))
+    assert broadcast(B.ones(5, 4), B.ones(5, 4)) == s
+    assert broadcast(B.ones(5, 4), B.ones(5, 1)) == s
+
+    with pytest.raises(AssertionError):
+        broadcast(B.ones(5, 5), B.ones(5, 4))
+    with pytest.raises(RuntimeError):
+        broadcast(Dimension(5), Dimension(4))
