760f8ef6ec3ce83e23ef50d5933c0e97d4791252
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-19 17:29:28 +0100

    Add dtype to str utility

--- matrix/util.py
+++ matrix/util.py
@@ -1,4 +1,7 @@
-__all__ = ['indent']
+import lab as B
+
+__all__ = ['indent',
+           'dtype_str']
 
 
 def indent(x, indentation='  '):
@@ -12,3 +15,19 @@ def indent(x, indentation='  '):
         str: `x` indented.
     """
     return indentation + x.replace('\n', '\n' + indentation)
+
+
+def dtype_str(x):
+    """Get the data type of an object as string.
+
+    Args:
+        x (tensor): Tensor to get data type of.
+
+    Returns:
+        str: Data type of `x`.
+    """
+    dtype = B.dtype(x)
+    if isinstance(dtype, type):
+        return dtype.__name__
+    else:
+        return str(dtype)
