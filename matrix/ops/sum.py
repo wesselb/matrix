aefa3a2f3b6647dd44b9be78ac32ac9b41390ef4
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-21 23:59:31 +0100

    Add sum

---
+++ matrix/ops/sum.py
@@ -0,0 +1,79 @@
+import lab as B
+
+from ..constant import Zero, Constant
+from ..diagonal import Diagonal
+from ..lowrank import LowRank
+from ..matrix import Dense
+from ..woodbury import Woodbury
+from ..kronecker import Kronecker
+
+__all__ = []
+
+
+def _raise(axis):
+    raise ValueError(f'Cannot sum over axis {axis}.')
+
+
+@B.dispatch(Zero)
+def sum(a, axis=None):
+    if axis not in [None, 0, 1]:
+        _raise(axis)
+    return B.cast(a.dtype, 0)
+
+
+@B.dispatch(Dense)
+def sum(a, axis=None):
+    return B.sum(a.mat, axis=axis)
+
+
+@B.dispatch(Diagonal)
+def sum(a, axis=None):
+    if axis is None:
+        return B.sum(a.diag)
+    elif axis == 0 or axis == 1:
+        return a.diag
+    else:
+        _raise(axis)
+
+
+@B.dispatch(Constant)
+def sum(a, axis=None):
+    if axis is None:
+        return a.const * a.rows * a.cols
+    elif axis == 0:
+        return a.const * a.rows * B.ones(B.dtype(a.const), a.cols)
+    elif axis == 1:
+        return a.const * a.cols * B.ones(B.dtype(a.const), a.rows)
+    else:
+        _raise(axis)
+
+
+@B.dispatch(LowRank)
+def sum(a, axis=None):
+    if axis is None:
+        return B.sum(B.sum(a.left, axis=0) * B.sum(a.right, axis=0))
+    elif axis == 0:
+        return B.sum(B.expand_dims(B.sum(a.left, axis=0), axis=0) * a.right,
+                     axis=1)
+    elif axis == 1:
+        return B.sum(a.left * B.expand_dims(B.sum(a.right, axis=0), axis=0),
+                     axis=1)
+    else:
+        _raise(axis)
+
+
+@B.dispatch(Woodbury)
+def sum(a, axis=None):
+    return B.sum(a.diag, axis=axis) + B.sum(a.lr, axis=axis)
+
+
+@B.dispatch(Kronecker)
+def sum(a, axis=None):
+    if axis is None:
+        return B.sum(a.left) * B.sum(a.right)
+    elif axis == 0:
+        return B.kron(B.sum(a.left, axis=0), B.sum(a.right, axis=0))
+    elif axis == 1:
+        return B.kron(B.sum(a.left, axis=1), B.sum(a.right, axis=1))
+    else:
+        _raise(axis)
---
+++ tests/ops/test_sum.py
@@ -0,0 +1,56 @@
+import pytest
+import lab as B
+
+from matrix import Dense, Diagonal, Constant, LowRank, Woodbury, Kronecker, Zero
+# noinspection PyUnresolvedReferences
+from ..util import (
+    allclose,
+    check_un_op,
+
+    zero_r,
+    dense_r,
+    diag1,
+    const_r,
+    lr_r,
+    wb1,
+    kron_r
+)
+
+
+def _check_sum(a):
+    for axis in [None, 0, 1]:
+        def sum(a):
+            return B.sum(a, axis=axis)
+
+        check_un_op(sum, a)
+
+    with pytest.raises(ValueError):
+        B.sum(a, axis=2)
+
+
+def test_sum_zero(zero_r):
+    _check_sum(zero_r)
+
+
+def test_sum_dense(dense_r):
+    _check_sum(dense_r)
+
+
+def test_sum_diag(diag1):
+    _check_sum(diag1)
+
+
+def test_sum_const(const_r):
+    _check_sum(const_r)
+
+
+def test_sum_lr(lr_r):
+    _check_sum(lr_r)
+
+
+def test_sum_wb(wb1):
+    _check_sum(wb1)
+
+
+def test_sum_kron(kron_r):
+    _check_sum(kron_r)
