d484b12cd424e94e196c9b1763384bc343fc0aed
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-24 13:05:01 +0100

    Test for Zero and Constant attributes

--- tests/test_constant.py
+++ tests/test_constant.py
@@ -1,4 +1,16 @@
-from matrix import Constant, Zero
+from matrix import Zero, Constant
+
+
+def test_zero_formatting():
+    assert str(Zero(int, 3, 3)) == '<zero matrix: shape=3x3, dtype=int>'
+    assert repr(Zero(int, 3, 3)) == '<zero matrix: shape=3x3, dtype=int>'
+
+
+def test_zero_attributes():
+    zero = Zero(int, 3, 4)
+    assert zero.dtype == int
+    assert zero.rows == 3
+    assert zero.cols == 4
 
 
 def test_constant_formatting():
@@ -8,6 +20,8 @@ def test_constant_formatting():
            '<constant matrix: shape=3x3, dtype=int, const=1>'
 
 
-def test_zero_formatting():
-    assert str(Zero(int, 3, 3)) == '<zero matrix: shape=3x3, dtype=int>'
-    assert repr(Zero(int, 3, 3)) == '<zero matrix: shape=3x3, dtype=int>'
+def test_constant_attributes():
+    const = Constant(1, 3, 4)
+    assert const.const == 1
+    assert const.rows == 3
+    assert const.cols == 4
