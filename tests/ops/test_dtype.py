a5b15addbf20e89e86e8988145e9295b1e0f8427
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-24 13:57:24 +0100

    Test dtype for LowerTriangular and UpperTriangular

--- tests/ops/test_dtype.py
+++ tests/ops/test_dtype.py
@@ -9,6 +9,8 @@ from ..util import (
     dense1,
     diag1,
     const1,
+    lt1,
+    ut1,
     lr1,
     wb1,
     kron1
@@ -29,6 +31,14 @@ def test_dtype_diag(diag1):
 
 def test_dtype_const(const1):
     assert B.dtype(const1) == B.default_dtype
+
+
+def test_dtype_lt1(lt1):
+    assert B.dtype(lt1) == B.default_dtype
+
+
+def test_dtype_ut1(ut1):
+    assert B.dtype(ut1) == B.default_dtype
 
 
 def test_dtype_lr(lr1):
