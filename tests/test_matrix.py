37adb1e3f5d33688c017d75eed74ed850139d895
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-24 13:10:35 +0100

    Test Dense attributes

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -32,3 +32,9 @@ def test_dense_formatting():
            ' mat=[[1. 1. 1.]\n' \
            '      [1. 1. 1.]\n' \
            '      [1. 1. 1.]]>'
+
+
+def test_dense_attributes():
+    mat = B.ones(3, 3)
+    dense = Dense(mat)
+    assert dense.mat is mat
