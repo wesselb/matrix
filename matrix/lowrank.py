f46a27b47c834800ec92ffc9bf07bd1be132b679
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-24 22:50:49 +0100

    Add middle to LowRank

--- matrix/lowrank.py
+++ matrix/lowrank.py
@@ -1,8 +1,7 @@
 import lab as B
-import wbml.out
 
 from .matrix import AbstractMatrix, repr_format
-from .shape import assert_matrix
+from .shape import assert_matrix, assert_square
 from .util import indent, dtype_str
 
 __all__ = ['LowRank']
@@ -13,14 +12,16 @@ class LowRank(AbstractMatrix):
 
     The data type of a low-rank matrix is the data type of the left factor.
     The Cholesky decomposition is not exactly the Cholesky decomposition,
-    but returns a matrix `L` such that `L transpose(L)` is the original martix.
+    but returns a matrix `L` such that `L transpose(L)` is the original matrix.
 
     Attributes:
         left (matrix): Left factor.
+        middle (matrix): Middle factor.
         right (matrix): Right factor.
         rank (int): Rank of the low-rank matrix.
-        symmetric (bool): Boolean indicating whether this is a symmetric
-            low-rank matrix.
+        symmetric (bool): Boolean indicating whether this the left and right
+            factor are identical. Note that this attribute does not check
+            whether the middle factor is symmetric.
         cholesky (:class:`.matrix.AbstractMatrix` or None): Cholesky-like
             decomposition of the matrix, once it has been computed.
         dense (matrix or None): Dense version of the matrix, once it has been
@@ -28,27 +29,37 @@ class LowRank(AbstractMatrix):
 
     Args:
         left (matrix): Left factor.
+        middle (matrix, optional): Middle factor. Defaults to the identity
+            matrix.
         right (matrix, optional): Right factor. Defaults to the left factor.
     """
 
-    def __init__(self, left, right=None):
-        if right is None:
-            check_factors = [left]
-        else:
-            check_factors = [left, right]
-        for factor in check_factors:
-            assert_matrix(factor, 'Factors are not rank-2 tensors. Can only '
-                                  'construct low-rank matrices from matrix '
-                                  'factors.')
+    def __init__(self, left, right=None, middle=None):
         self.left = left
-        self.right = left if right is None else right
         self.rank = B.shape(self.left)[1]
+        self.right = left if right is None else right
+        if middle is None:
+            self.middle = B.fill_diag(B.one(self.left), self.rank)
+        else:
+            self.middle = middle
         self.symmetric = right is None
         self.cholesky = None
         self.dense = None
 
-        if not B.shape(self.left)[1] == B.shape(self.right)[1]:
-            raise ValueError('Left and right factor must have an equal number '
+        msg = 'Can only construct low-rank matrices from matrix factors and ' \
+              'the middle factor must be square.'
+        assert_matrix(self.left,
+                      f'Left factor is not a rank-2 tensor. {msg}')
+        assert_square(self.middle,
+                      f'Middle factor is not a square rank-2 tensor. {msg}')
+        assert_matrix(self.right,
+                      f'Right factor is not a rank-2 tensor. {msg}')
+
+        if (
+                B.shape(self.middle)[1] != self.rank or
+                B.shape(self.right)[1] != self.rank
+        ):
+            raise ValueError('All factors must have an equal number '
                              'of columns.')
 
     def __str__(self):
@@ -63,5 +74,7 @@ class LowRank(AbstractMatrix):
         return str(self)[:-1] + '\n' + \
                f' left=' + indent(repr_format(self.left),
                                   ' ' * 6).strip() + '\n' + \
+               f' middle=' + indent(repr_format(self.middle),
+                                    ' ' * 8).strip() + '\n' + \
                f' right=' + indent(repr_format(self.right),
                                    ' ' * 7).strip() + '>'
--- tests/test_lowrank.py
+++ tests/test_lowrank.py
@@ -2,35 +2,46 @@ import lab as B
 import pytest
 from matrix import LowRank
 
+from .util import allclose
+
 
 def test_lowrank_formatting():
     assert str(LowRank(B.ones(3, 1), 2 * B.ones(3, 1))) == \
            '<low-rank matrix: shape=3x3, dtype=float64, rank=1,' \
            ' symmetric=False>'
-    assert repr(LowRank(B.ones(3, 1), 2 * B.ones(3, 1))) == \
-           '<low-rank matrix: shape=3x3, dtype=float64, rank=1,' \
+    assert repr(LowRank(B.ones(3, 2), 2 * B.ones(3, 2))) == \
+           '<low-rank matrix: shape=3x3, dtype=float64, rank=2,' \
            ' symmetric=False\n' \
-           ' left=[[1.]\n' \
-           '       [1.]\n' \
-           '       [1.]]\n' \
-           ' right=[[2.]\n' \
-           '        [2.]\n' \
-           '        [2.]]>'
+           ' left=[[1. 1.]\n' \
+           '       [1. 1.]\n' \
+           '       [1. 1.]]\n' \
+           ' middle=<diagonal matrix: shape=2x2, dtype=float64\n' \
+           '         diag=[1. 1.]>\n' \
+           ' right=[[2. 2.]\n' \
+           '        [2. 2.]\n' \
+           '        [2. 2.]]>'
 
 
 def test_lowrank_attributes():
+    # Check the case where the left and right factor are given.
     left = B.ones(3, 1)
     right = 2 * B.ones(3, 1)
     lr = LowRank(left, right)
     assert lr.left is left
+    allclose(B.dense(lr.middle), B.ones(1, 1))
     assert lr.right is right
     assert lr.rank == 1
     assert not lr.symmetric
 
-    lr = LowRank(left)
+    # Check the case where the left and middle factor are given.
+    middle = B.ones(2, 2)
+    lr = LowRank(B.ones(3, 2), middle=middle)
     assert lr.symmetric
+    assert lr.middle is middle
 
 
-def test_lowrank_col_check():
+def test_lowrank_shape_checks():
     with pytest.raises(ValueError):
         LowRank(B.ones(3, 1), B.ones(3, 2))
+    with pytest.raises(ValueError):
+        LowRank(B.ones(3, 1), B.ones(3, 1), B.eye(2))
