cac7aa4a91458323223cb5ed136cdb5716f2dfb7
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-24 13:06:18 +0100

    Test Diagonal attributes

--- tests/test_diagonal.py
+++ tests/test_diagonal.py
@@ -1,5 +1,7 @@
-from matrix import Diagonal
 import lab as B
+
+from matrix import Diagonal
+from .util import allclose
 
 
 def test_diagonal_formatting():
@@ -8,3 +10,8 @@ def test_diagonal_formatting():
     assert repr(Diagonal(B.ones(3))) == \
            '<diagonal matrix: shape=3x3, dtype=float64\n' \
            ' diag=[1. 1. 1.]>'
+
+
+def test_diagonal_attributes():
+    diag = Diagonal(B.ones(3))
+    allclose(diag.diag, B.ones(3))
