aac1d1075f6765dabe9577b730022f8e8703258f
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-24 13:07:28 +0100

    Test Kronecker attributes

--- tests/test_kronecker.py
+++ tests/test_kronecker.py
@@ -3,13 +3,21 @@ from matrix import Diagonal, Kronecker
 
 
 def test_kronecker_formatting():
-    d1 = Diagonal(B.ones(2))
-    d2 = Diagonal(B.ones(3))
-    assert str(Kronecker(d1, d2)) == \
+    left = Diagonal(B.ones(2))
+    right = Diagonal(B.ones(3))
+    assert str(Kronecker(left, right)) == \
            '<Kronecker product: shape=6x6, dtype=float64>'
-    assert repr(Kronecker(d1, d2)) == \
+    assert repr(Kronecker(left, right)) == \
            '<Kronecker product: shape=6x6, dtype=float64\n' \
            ' left=<diagonal matrix: shape=2x2, dtype=float64\n' \
            '       diag=[1. 1.]>\n' \
            ' right=<diagonal matrix: shape=3x3, dtype=float64\n' \
            '        diag=[1. 1. 1.]>>'
+
+
+def test_kronecker_attributes():
+    left = Diagonal(B.ones(2))
+    right = Diagonal(B.ones(3))
+    kron = Kronecker(left, right)
+    assert kron.left is left
+    assert kron.right is right
