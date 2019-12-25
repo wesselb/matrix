951577bff4ab4fe6d79f0003d82b28da1844cf4a
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-25 13:34:08 +0100

    Add concat

---
+++ matrix/ops/concat.py
@@ -0,0 +1,18 @@
+import warnings
+import lab as B
+
+from ..matrix import AbstractMatrix, Dense, structured
+from ..util import ToDenseWarning
+
+__all__ = []
+
+
+@B.dispatch([AbstractMatrix])
+def concat(*elements, axis=0):
+    if structured(*elements):
+        elements_str = ', '.join(map(str, elements[:3]))
+        if len(elements) > 3:
+            elements_str += '...'
+        warnings.warn(f'Concatenating {elements_str}: converting to dense.',
+                      category=ToDenseWarning)
+    return Dense(B.concat(*(B.dense(el) for el in elements), axis=axis))
---
+++ tests/ops/test_concat.py
@@ -0,0 +1,24 @@
+import lab as B
+
+from matrix import Dense
+
+# noinspection PyUnresolvedReferences
+from ..util import (
+    allclose,
+    check_un_op,
+    AssertDenseWarning,
+
+    dense1,
+    dense2,
+    diag1,
+    diag2
+)
+
+
+def test_concat(dense1, dense2, diag1, diag2):
+    with AssertDenseWarning('concatenating <dense>, <dense>, <diagonal>...'):
+        res = B.concat(dense1, dense2, diag1, diag2, axis=1)
+        dense_res = B.concat(B.dense(dense1), B.dense(dense2),
+                             B.dense(diag1), B.dense(diag2), axis=1)
+        allclose(res, dense_res)
+        assert isinstance(res, Dense)
