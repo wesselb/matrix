4c018874ffbafd5478954c8247d79f74035ca60d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-26 12:17:19 +0100

    Add root

---
+++ matrix/ops/root.py
@@ -0,0 +1,71 @@
+import warnings
+
+import lab as B
+
+from ..constant import Zero, Constant
+from ..diagonal import Diagonal
+from ..kronecker import Kronecker
+from ..lowrank import LowRank
+from ..matrix import Dense
+from ..shape import assert_square
+from ..util import ToDenseWarning
+from ..woodbury import Woodbury
+
+__all__ = []
+
+
+def _assert_square_root(a):
+    assert_square(a, 'Can only take a square root of square matrices.')
+
+
+@B.dispatch(B.Numeric)
+def root(a):  # pragma: no cover
+    """Compute the positive square root of a positive-definite matrix.
+
+    Args:
+        a (matrix): Matrix to compute square root of.
+
+    Returns:
+        matrix: Positive square root of `a`.
+    """
+    _assert_square_root(a)
+    u, s, _ = B.svd(a)
+    return B.mm(B.mm(u, B.diag(B.sqrt(s))), u, tr_b=True)
+
+
+B.root = root
+
+
+@B.dispatch(Zero)
+def root(a):
+    _assert_square_root(a)
+    return a
+
+
+@B.dispatch(Dense)
+def root(a):
+    return Dense(B.root(B.dense(a)))
+
+
+@B.dispatch(Diagonal)
+def root(a):
+    _assert_square_root(a)
+    return B.cholesky(a)
+
+
+@B.dispatch(Constant)
+def root(a):
+    _assert_square_root(a)
+    return B.cholesky(a)
+
+
+@B.dispatch({LowRank, Woodbury})
+def root(a):
+    warnings.warn(f'Converting {a} to dense to compute its square root.',
+                  category=ToDenseWarning)
+    return Dense(B.root(B.dense(a)))
+
+
+@B.dispatch(Kronecker)
+def root(a):
+    return Kronecker(B.root(a.left), B.root(a.right))
---
+++ tests/ops/test_root.py
@@ -0,0 +1,64 @@
+import lab as B
+import pytest
+
+from matrix import Dense, Constant, Diagonal, Kronecker, LowerTriangular
+# noinspection PyUnresolvedReferences
+from ..util import (
+    allclose,
+    check_un_op,
+    AssertDenseWarning,
+
+    zero1,
+    dense_pd,
+    diag_pd,
+    const_pd,
+    lr_pd,
+    lr1,
+    wb_pd,
+    kron_pd
+)
+
+
+def _check_root(a, asserted_type=object):
+    root = B.root(a)
+
+    # Check correctness.
+    allclose(B.matmul(B.dense(root), B.dense(root)), B.dense(a))
+
+    # Check type.
+    assert isinstance(root, asserted_type)
+
+
+def test_root_square_assertion():
+    with pytest.raises(AssertionError):
+        B.root(Dense(B.randn(3, 4)))
+
+
+def test_root_zero(zero1):
+    assert B.root(zero1) is zero1
+
+
+def test_root_dense(dense_pd):
+    _check_root(dense_pd, asserted_type=Dense)
+
+
+def test_root_diag(diag_pd):
+    _check_root(diag_pd, asserted_type=Diagonal)
+
+
+def test_root_const(const_pd):
+    _check_root(const_pd, asserted_type=Constant)
+
+
+def test_root_lr(lr_pd):
+    with AssertDenseWarning('converting <low-rank> to dense'):
+        _check_root(lr_pd, asserted_type=Dense)
+
+
+def test_root_wb(wb_pd):
+    with AssertDenseWarning('converting <woodbury> to dense'):
+        _check_root(wb_pd, asserted_type=Dense)
+
+
+def test_root_kron(kron_pd):
+    _check_root(kron_pd, asserted_type=Kronecker)
