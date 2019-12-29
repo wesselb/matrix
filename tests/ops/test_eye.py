51fb5b06125428c5c8451d6f3636d2da16161feb
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-12-29 14:00:49 +0100

    Also test for type in eye

--- tests/ops/test_eye.py
+++ tests/ops/test_eye.py
@@ -1,9 +1,11 @@
 import lab as B
 import numpy as np
 
-# noinspect PyUnresolvedReferences
+from matrix import Diagonal
+# noinspection PyUnresolvedReferences
 from ..util import allclose, dense1
 
 
 def test_eye(dense1):
     allclose(B.eye(dense1), np.eye(B.shape(dense1)[0]))
+    assert isinstance(B.eye(dense1), Diagonal)
