2b07f9fdc2f52508f5147f7a6deb53af08b66d9c
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-12-05 19:30:28 +0000

    Increase version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 requirements = [
     "numpy>=1.16",
     "plum-dispatch>=0.2.3",
-    "backends>=0.4.5",
+    "backends>=0.5.0",
     "wbml>=0.1.3",
     "algebra",
 ]
