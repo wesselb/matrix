86c42cf2144620696b75d9917da0407cf6581d95
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-09-04 15:40:17 +0200

    Update dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 requirements = [
     "numpy>=1.16",
     "plum-dispatch>=1.5.3",
-    "backends>=1.4.3",
+    "backends>=1.4.4",
     "wbml>=0.3",
     "algebra",
 ]
