14736a6565c52aa06a0e7cad542a29020e3fe031
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-10-27 22:43:39 +0200

    Add `rank` keyword for `uprank`

--- matrix/ops/uprank.py
+++ matrix/ops/uprank.py
@@ -6,5 +6,7 @@ __all__ = []
 
 
 @B.dispatch
-def uprank(a: AbstractMatrix):
+def uprank(a: AbstractMatrix, rank: B.Int = 2):
+    if rank != 2:
+        raise ValueError("Can only uprank a matrix to rank two.")
     return a
--- tests/ops/test_uprank.py
+++ tests/ops/test_uprank.py
@@ -1,3 +1,4 @@
+import pytest
 import lab as B
 
 # noinspection PyUnresolvedReferences
@@ -6,3 +7,9 @@ from ..util import approx, check_un_op, dense1
 
 def test_uprank_dense(dense1):
     assert B.uprank(dense1) is dense1
+
+    # Test that `rank` can be given as a keyword argument, but must also be set to `2`.
+    B.uprank(dense1, rank=2)
+    with pytest.raises(ValueError):
+        B.uprank(dense1, rank=3)
+
